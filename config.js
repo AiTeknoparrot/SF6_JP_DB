3e89e496773999b12d71a6336dae90a55fe27aaf
AiTeknoparrot <64667348+AiTeknoparrot@users.noreply.github.com>
2023-11-21 00:01:58 -0500

    Added db files

---
+++ config.js
@@ -0,0 +1,23 @@
+module.exports = {
+	"port": 8888,
+	"matchData": "data/match.data",
+	"reportsFilename": "data/reports.data",
+	"logFilename": "data/log-DATE.data",
+	"resultsPerPage": 50,
+	"server": "./server/index.js",
+	"contributors": [
+		{
+			"name": "nameless",
+			"html": "<p>No comment.</p>",
+			"img": "/img/someone.png",
+			"token": "yurihime9"
+		},
+		{
+			"name": "Crablets",
+			"html": "<p>In gratitude for all beautiful culinary creations.</p>",
+			"img": "/img/someone.png",
+			"token": "shrimple8055",
+			"noApprove": true
+		},
+	]
+};
---
+++ server/charIdMap.js
@@ -0,0 +1,122 @@
+module.exports = {
+	"c1": {
+		"id": "c1",
+		"name": "A.K.I.",
+		"full": "A.K.I.",
+		"wiki": "https://wiki.supercombo.gg/w/Street_Fighter_6/A.K.I."
+	},
+	"c2": {
+		"id": "c2",
+		"name": "Blanka",
+		"full": "Blanka",
+		"wiki": "https://wiki.supercombo.gg/w/Street_Fighter_6/Blanka"
+	},
+	"c3": {
+		"id": "c3",
+		"name": "Cammy",
+		"full": "Cammy",
+		"wiki": "https://wiki.supercombo.gg/w/Street_Fighter_6/Cammy"
+	},
+	"c4": {
+		"id": "c4",
+		"name": "Chun-Li",
+		"full": "Chun-Li",
+		"wiki": "https://wiki.supercombo.gg/w/Street_Fighter_6/Chun-Li"
+	},
+	"c5": {
+		"id": "c5",
+		"name": "Dee Jay",
+		"full": "Dee Jay",
+		"wiki": "https://wiki.supercombo.gg/w/Street_Fighter_6/Dee_Jay"
+	},
+	"c6": {
+		"id": "c6",
+		"name": "Dhalsim",
+		"full": "Dhalsim",
+		"wiki": "https://wiki.supercombo.gg/w/Street_Fighter_6/Dhalsim"
+	},
+	"c7": {
+		"id": "c7",
+		"name": "E.Honda",
+		"full": "E.Honda",
+		"wiki": "https://wiki.supercombo.gg/w/Street_Fighter_6/E.Honda"
+	},
+	"c8": {
+		"id": "c8",
+		"name": "Guile",
+		"full": "Guile",
+		"wiki": "https://wiki.supercombo.gg/w/Street_Fighter_6/Guile"
+	},
+	"c9": {
+		"id": "c9",
+		"name": "Jamie",
+		"full": "Jamie",
+		"wiki": "https://wiki.supercombo.gg/w/Street_Fighter_6/Jamie"
+	},
+	"c10": {
+		"id": "c10",
+		"name": "JP",
+		"full": "JP",
+		"wiki": "https://wiki.supercombo.gg/w/Street_Fighter_6/JP"
+	},
+	"c11": {
+		"id": "tmk",
+		"name": "Juri",
+		"full": "Juri",
+		"wiki": "https://wiki.supercombo.gg/w/Street_Fighter_6/Juri"
+	},
+	"c12": {
+		"id": "c12",
+		"name": "Ken",
+		"full": "Ken",
+		"wiki": "https://wiki.supercombo.gg/w/Street_Fighter_6/Ken"
+	},
+	"c13": {
+		"id": "c13",
+		"name": "Kimberly",
+		"full": "Kimberly",
+		"wiki": "https://wiki.supercombo.gg/w/Street_Fighter_6/Kimberly"
+	},
+	"c14": {
+		"id": "c14",
+		"name": "Lily",
+		"full": "Lily",
+		"wiki": "https://wiki.supercombo.gg/w/Street_Fighter_6/Lily"
+	},
+	"c15": {
+		"id": "c15",
+		"name": "Luke",
+		"full": "Luke",
+		"wiki": "https://wiki.supercombo.gg/w/Street_Fighter_6/Luke"
+	},
+	"c16": {
+		"id": "c16",
+		"name": "Manon",
+		"full": "Manon",
+		"wiki": "https://wiki.supercombo.gg/w/Street_Fighter_6/Manon"
+	},
+	"c17": {
+		"id": "c17",
+		"name": "Marisa",
+		"full": "Marisa",
+		"wiki": "https://wiki.supercombo.gg/w/Street_Fighter_6/Marisa"
+	},
+	"c18": {
+		"id": "c18",
+		"name": "Rashid",
+		"full": "Rashid",
+		"wiki": "https://wiki.supercombo.gg/w/Street_Fighter_6/Rashid"
+	},
+	"c19": {
+		"id": "c19",
+		"name": "Ryu",
+		"full": "Ryu",
+		"wiki": "https://wiki.supercombo.gg/w/Street_Fighter_6/Ryu"
+	},
+	"c20": {
+		"id": "c20",
+		"name": "Zangief",
+		"full": "Zangief",
+		"wiki": "https://wiki.supercombo.gg/w/Street_Fighter_6/Zangief"
+	},
+};
